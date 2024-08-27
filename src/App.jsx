f6d1df7a69ca647f24f06fad42ab5f1e2de8e83d
kaustubh3112 <kaustubhyeolekar>
2024-08-27 22:32:23 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
 
 function App() {
-  return <>sadsdasd</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
